dc21469f27e4b9479812d1b0d4cf88d782469f20
tracer-ics2017 <tracer@njuics.org>
2024-03-18 19:13:54 -0700

    >  compile 2113601 star Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:13:54 up 59 min,  3 users,  load average: 0.05, 0.02, 0.00 2fde6970e9ce1842634e10ff5eef9c3ee4ad096f

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,8 +8,15 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  int oldValue;
+  char e[32];
+  int hitNum;
 
 } WP;
+
+bool new_wp(char *arg);
+bool free_wp(int num);
+void print_wp();
+bool watch_wp();
 
 #endif
